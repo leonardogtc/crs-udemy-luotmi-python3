7eb54d0d5bffe66980701d26ff58750cc392af53
Leonardo Gonçalves Tavares da Conceição <leonardogtc@gmail.com>
2024-01-07 08:49:52 -0300

    Seção de Funções Inicializada

--- sec004-funcoes/v106-main-arg-nomeado-posicional.py
+++ sec004-funcoes/v106-main-arg-nomeado-posicional.py
@@ -5,12 +5,14 @@ Argumento não nomeado recebe apenas o argumento (valor)
 """
 
 
-def soma(x, y):
+# ? def soma(x, y):
+def soma(x, y, z):
     # Definição
     # Mostra somente o resultado: print(x + y)
     # Mostra os argumentos da forma x=1 + y=2: print(f'{x=} + {y=}')
 
-    print(f'{x=} + {y=}')
+    # ? print(f'{x=} + {y=}')
+    print(f'{x=} y={y} {z=}', '|', 'x + y + z = ', x + y + z)
 
 # A chamada de função dessa maneira imprime o resultado
 # e o texto None - indicando que a função não retorna
@@ -21,4 +23,13 @@ def soma(x, y):
 # Executando a função - Dessa maneira imprime somente o
 # resultado. Os argumentos passados são argumentos posicionais
 # que dependem da ordem em que são passados.
-soma(1, 2)
+# ? soma(1, 2)
+soma(1, 2, 3)
+
+# Argumento nomeado posicional - Não preciso me preocupar com
+# a ordem caso indique que parâmetro nomeado receberá o valor.
+# Toda vez que nomear um argumento tem que nomear todos os
+# subsequentes: soma(1, y=2, z=3)
+# ? soma(y=2, x=1)
+
+soma(y=2, z=3, x=1)
