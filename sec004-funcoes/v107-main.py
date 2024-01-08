4eb80678d9093df35f14eae051540315291bba99
Leonardo Gonçalves Tavares da Conceição <leonardogtc@gmail.com>
2024-01-08 13:20:18 -0300

    Teste após realizar um clone

---
+++ sec004-funcoes/v107-main.py
@@ -0,0 +1,8 @@
+"""
+Valores padrão para parâmetros
+Ao definir uma função, os parâmetros podem
+ter valores padrão. Caso o valor não seja
+enviado para o parâmetro, o valor padrão será
+usado.
+Refatorar: editar o seu código.
+"""
