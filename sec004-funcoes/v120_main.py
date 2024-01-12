d984278a9802401ca3c5967cb3db62eed5041f5a
Leonardo Gonçalves Tavares da Conceição <leonardogtc@gmail.com>
2024-01-12 05:40:22 -0300

    Vídeo aula 120 - completa

--- sec004-funcoes/v120_main.py
+++ sec004-funcoes/v120_main.py
@@ -1,29 +1,46 @@
 """
     Manipulando chaves e valores em dicionários
 """
+
+# Em alguns casos, mypy não consegue inferir o tipo de uma variável sem uma
+# anotação explícita. Mypy trata isso como um erro. Isso normalmente acontece
+# quando você inicializa uma variável com uma coleção vazia ou None. Se mypy
+# não puder inferir o tipo de item da coleção, mypy substituirá quaisquer
+# partes do tipo que não pôde inferir por Any e gerará um erro.
 pessoa = {}
 
-##
-##
+# Mostra uma lista vazia
+pessoa["nome"] = "Leonardo"
+pessoa["sobrenome"] = "Conceição"
 
-chave = "nome"
+# Quando a chave não existe gera uma KeyError:
+# print(pessoa['nome1'])
 
-pessoa[chave] = "Luiz Otávio"
-pessoa["sobrenome"] = "Miranda"
+# Imprimindo o dicionário
+print(pessoa)
 
+# Modificando o valor de uma chave dinamicamente
+pessoa["nome"] = "Lúcia"
 
-print(pessoa[chave])
+# Imprimindo
+print(pessoa["nome"], pessoa["sobrenome"])
 
-pessoa[chave] = "Maria"
-
+# Apagando uma chave
 del pessoa["sobrenome"]
 print(pessoa)
-print(pessoa["nome"])
+##
 
-# print(pessoa.get('sobrenome'))
+# O método get para obter uma chave e efetuar um teste.
+# Por padrão o método get retornará None.
+print(pessoa.get("sobrenome"))
+
+# Contornando a exceção com o método get do dicionário
 if pessoa.get("sobrenome") is None:
     print("NÃO EXISTE")
 else:
     print(pessoa["sobrenome"])
 
-# print('ISSO Não vai')
+# Criação de chave dinâmica
+chave = "idade"
+pessoa[chave] = "15"
+print(pessoa[chave])
